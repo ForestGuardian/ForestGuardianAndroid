4d2b752fb5da2186a82ad1ce39b64228d0df5f9b
Emmanuel Mora <emmanuelmora05@gmail.com>
2017-04-09 17:50:04 -0600

    Successful sign up loads MapActivity.

--- app/src/main/java/org/forestguardian/View/SignUpActivity.java
+++ app/src/main/java/org/forestguardian/View/SignUpActivity.java
@@ -1,5 +1,6 @@
 package org.forestguardian.View;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
@@ -7,8 +8,6 @@ import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
-import junit.framework.Assert;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -126,6 +125,10 @@ public class SignUpActivity  extends AppCompatActivity {
                         ForestGuardianService.global().addAuthenticationHeaders(authenticatedUser.getEmail(), accessToken);
 
                         Toast.makeText(this, "Welcome!", Toast.LENGTH_SHORT).show();
+
+                        Intent intent = new Intent(getApplicationContext(),MapActivity.class);
+                        startActivity(intent);
+                        finish();
                     });
 
         });
