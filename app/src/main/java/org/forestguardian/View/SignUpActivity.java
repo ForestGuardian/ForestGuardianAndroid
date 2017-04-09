f5b55716d04666c9038bfd151eee23b4e2dad7de
Emmanuel Mora <emmanuelmora05@gmail.com>
2017-04-08 19:08:31 -0600

    Add SignUpActivity.

---
+++ app/src/main/java/org/forestguardian/View/SignUpActivity.java
@@ -0,0 +1,13 @@
+package org.forestguardian.View;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by emma on 08/04/17.
+ */
+
+public class SignUpActivity  extends AppCompatActivity {
+
+
+
+}
