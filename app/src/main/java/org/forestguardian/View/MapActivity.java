8251986067ef2325e228bbfb54b917d238f6f77d
Emmanuel Mora <emmanuelmora05@gmail.com>
2017-04-09 20:36:03 -0600

    Remove no longer required items in navigation view.

--- app/src/main/java/org/forestguardian/View/MapActivity.java
+++ app/src/main/java/org/forestguardian/View/MapActivity.java
@@ -138,13 +138,7 @@ public class MapActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
+        if (id == R.id.nav_reportes) {
 
         } else if (id == R.id.nav_share) {
 
