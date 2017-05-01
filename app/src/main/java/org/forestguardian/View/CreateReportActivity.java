c3284e21faf69b36a8b9f650d29cb03247d21d71
Emmanuel Mora <emmanuelmora05@gmail.com>
2017-04-30 20:41:38 -0600

    Change onCreate definition.

--- app/src/main/java/org/forestguardian/View/CreateReportActivity.java
+++ app/src/main/java/org/forestguardian/View/CreateReportActivity.java
@@ -1,8 +1,6 @@
 package org.forestguardian.View;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import org.forestguardian.R;
@@ -14,8 +12,8 @@ import org.forestguardian.R;
 public class CreateReportActivity extends AppCompatActivity {
 
     @Override
-    public void onCreate(@Nullable final Bundle savedInstanceState, @Nullable final PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
         setContentView(R.layout.new_report_layout);
     }
