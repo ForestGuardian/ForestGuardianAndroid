21d132b7ff9a123b051339d76ecc3b9d5605d2f7
Emmanuel Mora <emmanuelmora05@gmail.com>
2017-07-10 11:14:54 -0600

    Add empty custom profile view class and TODO.

---
+++ app/src/main/java/org/forestguardian/View/CustomViews/ProfileImageView.java
@@ -0,0 +1,16 @@
+package org.forestguardian.View.CustomViews;
+
+import android.content.Context;
+import android.widget.LinearLayout;
+
+/**
+ * Created by emma on 10/07/17.
+ */
+
+//TODO: complete and replace profile views for this custom class.
+public class ProfileImageView extends LinearLayout{
+
+    public ProfileImageView(final Context context) {
+        super(context);
+    }
+}
