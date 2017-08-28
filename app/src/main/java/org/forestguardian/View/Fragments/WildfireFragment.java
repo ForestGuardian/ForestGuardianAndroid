338aa2c1f8749a0881dc9ef6c77fcbd6b4ca7b75
Luis Alonso Murillo Rojas <luismurirojas@gmail.com>
2017-08-27 19:13:08 -0600

    Add the text logic for the wildfire report screen

--- app/src/main/java/org/forestguardian/View/Fragments/WildfireFragment.java
+++ app/src/main/java/org/forestguardian/View/Fragments/WildfireFragment.java
@@ -1,15 +1,23 @@
 package org.forestguardian.View.Fragments;
 
 
+import android.location.Location;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.forestguardian.DataAccess.OSM.FireStation;
+import org.forestguardian.DataAccess.OSM.OverpassWrapper;
+import org.forestguardian.DataAccess.OSM.WaterResource;
+import org.forestguardian.Helpers.GeoHelper;
 import org.forestguardian.R;
+
+import java.io.IOException;
 
 import butterknife.BindView;
 
@@ -59,7 +67,99 @@ public class WildfireFragment extends Fragment {
         mLatitude = getArguments().getDouble(LATITUDE_KEY, -1);
         mLongitude = getArguments().getDouble(LONGITUDE_KEY, -1);
 
+        setWildfireData();
+
         return view;
+    }
+
+    private void setWildfireData() {
+        if (mLatitude != -1 && mLongitude != -1) {
+            // Create the wildfire location
+            Location wildfireLocation = new Location("");
+            wildfireLocation.setLatitude(mLatitude);
+            wildfireLocation.setLongitude(mLongitude);
+
+            // Search for the nearest fire station
+            OverpassWrapper overpassWrapper = new OverpassWrapper();
+            overpassWrapper.setOSMPoint(wildfireLocation);
+            overpassWrapper.getOSMDataForFireStations(100000, result -> {
+                if (result != null) {
+                    FireStation nearestFireStation = null;
+                    double tmpDistance = 0;
+
+                    for (int index = 0; index < result.elements.size(); index++) {
+                        //Initiate the fire station model
+                        FireStation fireStation = new FireStation();
+                        fireStation.setName(result.elements.get(index).tags.name);
+                        fireStation.setCity(result.elements.get(index).tags.addressCity);
+                        fireStation.setStreet(result.elements.get(index).tags.addressStreet);
+                        fireStation.setOperator(result.elements.get(index).tags.operator);
+                        fireStation.setCoordinate(result.elements.get(index).lat, result.elements.get(index).lon);
+
+                        //Set the address of the firestation
+                        new Thread(() -> {
+                            try {
+                                String fireStationAddress = GeoHelper.getAddressNameFromPoint(getActivity(), fireStation.getCoordinate());
+                                fireStation.setAddress(fireStationAddress);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }).start();
+
+                        //Set initial conditions
+                        if (nearestFireStation == null) {
+                            nearestFireStation = fireStation;
+                            tmpDistance = GeoHelper.calculateDistanceBetweenTwoPoints(wildfireLocation, fireStation.getCoordinate());
+                        }
+
+                        //Verify the distance between the wildfire and the fire stations
+                        double currentStationDistance = GeoHelper.calculateDistanceBetweenTwoPoints(wildfireLocation, fireStation.getCoordinate());
+                        if (currentStationDistance < tmpDistance) {
+                            nearestFireStation = fireStation;
+                            tmpDistance = currentStationDistance;
+                        }
+                    }
+                    //Print the nearest fire station information
+                    if (nearestFireStation != null) {
+                        final FireStation tmpNearestFireStation = nearestFireStation;
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                mFirefighters.setText(tmpNearestFireStation.getAddress());
+                            }
+                        });
+                    }
+                } else {
+                    Log.e(TAG, "Error getting the wildfires data!!");
+                }
+            });
+
+            // Search for water resources
+            overpassWrapper.getOSMDataForRivers(1000, result -> {
+                if (result != null) {
+                    if (result.elements.size() > 0) {
+                        final String waterName = result.elements.get(0).tags.name;
+
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                mWaterResource.setText(waterName);
+                            }
+                        });
+                    }
+                } else {
+                    Log.e(TAG, "Error getting the rivers data!!");
+                }
+            });
+
+            // Set the wildfire coordinates
+            getActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    mPosition.setText(GeoHelper.formatCoordinates(wildfireLocation));
+                }
+            });
+        }
     }
 
 }
